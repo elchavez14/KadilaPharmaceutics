796608da50f688e8d83d724aa2a7b6a0f1b6e138
elchavez14 <chatnoircatmua@gmail.com>
2023-09-25 10:55:02 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KadilaPharmaceutics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
